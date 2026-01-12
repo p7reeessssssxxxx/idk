db08513357290566742b120fca5c8c48851ec3ea
p7reeessssssxxxx <203464561+p7reeessssssxxxx@users.noreply.github.com>
2026-01-12 20:08:40 +0000

    Add script to check game ID and load content

---
+++ yen.lua
@@ -0,0 +1,5 @@
+if game.PlaceId == 130247632398296 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/p7reeessssssxxxx/idk/refs/heads/main/afentyen.lua"))()
+else
+    game.Players.LocalPlayer:Kick(" Wrong Game!\nThis script is for game ID: 130247632398296")
+end
